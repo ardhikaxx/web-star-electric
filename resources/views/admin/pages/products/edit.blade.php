3c8bff4fa06cfff7c0389f2472da855cfbce35f4
ardhikaxx <ardhikayanuar58@gmail.com>
2026-03-26 08:14:09 +0700

    fix: perbaiki layout page-header edit produk dengan justify-between

--- resources/views/admin/pages/products/edit.blade.php
+++ resources/views/admin/pages/products/edit.blade.php
@@ -326,7 +326,7 @@
             ],
         ])
 
-        <div class="page-header">
+        <div class="page-header d-flex align-items-center justify-content-between">
             <h2>
                 <i class="fa-solid fa-pen-nib"></i>
                 Edit Produk
