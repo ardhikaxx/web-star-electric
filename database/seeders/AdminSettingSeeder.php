3a1f1a4e57cb57c692eb1102531f61061558f153
ardhikaxx <ardhikayanuar58@gmail.com>
2026-03-26 10:38:49 +0700

    feat: tambahkan seeder untuk PIN admin default

---
+++ database/seeders/AdminSettingSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AdminSetting;
+use Illuminate\Database\Seeder;
+
+class AdminSettingSeeder extends Seeder
+{
+    public function run(): void
+    {
+        AdminSetting::updateOrCreate(
+            ['key' => 'admin_pin'],
+            ['value' => '1234']
+        );
+    }
+}
