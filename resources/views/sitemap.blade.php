7a9f355b52a0d8f530c61edbc2e7b3edb74f0285
ardhikaxx <ardhikayanuar58@gmail.com>
2026-03-31 20:08:59 +0700

    feat: perbaiki struktur sitemap dengan lastmod dan changefreq

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,12 +1,16 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>{{ route('home') }}</loc>
+        <loc>{{ url('/') }}</loc>
+        <lastmod>{{ now()->toIso8601ZuluString() }}</lastmod>
+        <changefreq>daily</changefreq>
         <priority>1.0</priority>
     </url>
-    @foreach ($products as $product)
+    @foreach ($products ?? [] as $product)
     <url>
-        <loc>{{ route('products.show', $product) }}</loc>
+        <loc>{{ route("products.show", $product->id) }}</loc>
+        <lastmod>{{ optional($product->updated_at)->toIso8601ZuluString() ?? now()->toIso8601ZuluString() }}</lastmod>
+        <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
     @endforeach
